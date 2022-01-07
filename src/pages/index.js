7827c7a0c3d76b36c6f7beb681474b59ec758525
yangliudev <ylyangliu1@gmail.com>
2022-01-06 21:03:43 -0800

    added bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,99 @@
 import * as React from "react";
 
 import styled from "styled-components";
+import { Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const IndexPage = () => {
   return (
-    <main>
+    <div>
       <title>Yang Liu</title>
-      <Container>
-        <Text>Hi, my name is Yang Liu</Text>
-        <Text>I am a Software Developer</Text>
-      </Container>
-      <div>
+      <TitleDiv>
+        <Header>Hi, my name is Yang Liu</Header>
+        <SubHeader>I am a Software Developer</SubHeader>
+      </TitleDiv>
+      <PersonalProjectsDiv>
         <Text>Work Experience</Text>
-      </div>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          feugiat sem erat. Vestibulum ultricies felis eleifend pretium mollis.
+          Nunc facilisis, tortor eu euismod lacinia, lorem erat porta velit, et
+          sagittis justo turpis vitae turpis. Nulla sapien mi, dictum vel quam
+          ac, posuere consectetur nunc. Ut quam nisi, imperdiet ut risus at,
+          dictum iaculis nisl. Aenean urna lacus, condimentum posuere aliquam
+          sed, elementum vitae eros. Nulla nec ipsum convallis, pulvinar leo
+          maximus, condimentum ante.
+        </Text>
+      </PersonalProjectsDiv>
       <div>
         <Text>Personal Projects</Text>
+        <Row>
+          <Col sm={12} md={6}>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              feugiat sem erat. Vestibulum ultricies felis eleifend pretium
+              mollis. Nunc facilisis, tortor eu euismod lacinia, lorem erat
+              porta velit, et sagittis justo turpis vitae turpis. Nulla sapien
+              mi, dictum vel quam ac, posuere consectetur nunc. Ut quam nisi,
+              imperdiet ut risus at, dictum iaculis nisl. Aenean urna lacus,
+              condimentum posuere aliquam sed, elementum vitae eros. Nulla nec
+              ipsum convallis, pulvinar leo maximus, condimentum ante.
+            </Text>
+          </Col>
+          <Col sm={12} md={6}>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              feugiat sem erat. Vestibulum ultricies felis eleifend pretium
+              mollis. Nunc facilisis, tortor eu euismod lacinia, lorem erat
+              porta velit, et sagittis justo turpis vitae turpis. Nulla sapien
+              mi, dictum vel quam ac, posuere consectetur nunc. Ut quam nisi,
+              imperdiet ut risus at, dictum iaculis nisl. Aenean urna lacus,
+              condimentum posuere aliquam sed, elementum vitae eros. Nulla nec
+              ipsum convallis, pulvinar leo maximus, condimentum ante.
+            </Text>
+          </Col>
+        </Row>
       </div>
       <div>
         <Text>Education</Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          feugiat sem erat. Vestibulum ultricies felis eleifend pretium mollis.
+          Nunc facilisis, tortor eu euismod lacinia, lorem erat porta velit, et
+          sagittis justo turpis vitae turpis. Nulla sapien mi, dictum vel quam
+          ac, posuere consectetur nunc. Ut quam nisi, imperdiet ut risus at,
+          dictum iaculis nisl. Aenean urna lacus, condimentum posuere aliquam
+          sed, elementum vitae eros. Nulla nec ipsum convallis, pulvinar leo
+          maximus, condimentum ante.
+        </Text>
       </div>
-    </main>
+    </div>
   );
 };
 
 // Styled Components
 
-const Container = styled.div`
+const TitleDiv = styled.div`
   margin: 3rem auto;
   max-width: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const PersonalProjectsDiv = styled.div`
+  width: 50%;
+`;
+
+const Header = styled.p`
+  font-family: "Montserrat";
+  font-size: 2em;
+`;
+
+const SubHeader = styled.p`
+  font-family: "Montserrat";
+  font-size: 1.2em;
 `;
 
 const Text = styled.p`
